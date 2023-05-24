75804560e39063cb2f5bba506b54231c87bb120c
Solanki Nikhil <133026873+nikhil16898@users.noreply.github.com>
2023-05-24 22:37:26 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# # # a = int(input("enter number"))
+# # if 20:
+# #     print ("positiv")
+# # elif a==0:
+# #     print("zero")
+# # else:
+# #     print("nagativ")
+# # print (int(False))
+
+# contries = ['india', 'aus', 'usa']
+# for contry in contries:
+#     print(contry)
+
+a = []
+for i in range(1,5):
+    a.append(i)
+print(a)
---
+++ monday_class.py
@@ -0,0 +1,11 @@
+# i = 1
+# while i<=5:
+#     print(i)
+#     i+=1
+
+def my_function(name):
+    print("welcome ",name)
+    
+print("Prongram Starting")
+my_function("Nikhil")
+print("end of program")
